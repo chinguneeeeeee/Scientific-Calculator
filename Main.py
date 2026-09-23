006c0b64e88895dc50c2c657019ba299d9d7b063
chingunee Calculator <ctuguldur3036@eagle.fgcu.edu>
2026-09-23 14:24:15 -0400

    Additional changes

--- Main.py
+++ Main.py
@@ -3,9 +3,21 @@
 #Sources: None
 #GitHub Link: https://github.com/chinguneeeeeee/Scientific-Calculator/
 
+def listOfFunc():
+    # Print the lists of function that the user want to use
+    print("Lists of functions:")
+    print("1. Basic Equations\n"
+          "2. Combinatorics\n"
+          "3. Calculus\n"
+          "4. Settings\n"
+          "5. Exit the calculator\n")
+
 def firstFunction(equations):
     # Return the value of the user's equation
+    print()
     print(equations, "=", eval(equations))
+    print()
+
 
 def secondFunction():
     return None
@@ -17,46 +29,54 @@ def settingsFunction():
     return None
 
 def exitFunction():
+    print() # New line
     print("Goodbye! Have a nice day!")
+    print()
 
+if __name__ == "__main__":
+    # Asking the user's name for the introductory part
+    #name = input("Enter your name: ")
 
+    # Introduction
+    #print("Hello ", name, ", this is a scientific calculator.", sep='')
+    conditionToUse = int(input(
+        "Press 1 if you want to use the calculator: "
+    )) # Ask the user's consent to use the calculator
 
-# Asking the user's name for the introductory part
-name = input("Enter your name: ")
+    if conditionToUse == 1:
+        # Header of the program
+        print("=" * 30 + " CALCULATOR " + "=" * 30, "\n")
 
-#print("Hello ", name, ", this is a scientific calculator.", sep='') # Introduction
-conditionToUse = int(input("Press 1 if you want to use the calculator: ")) # Ask the user's consent to use the calculator
-print() # New line
+        # Initialize the variable
+        functionToUse = 0
 
-if conditionToUse == 1:
-    print("=" * 30 + " CALCULATOR " + "=" * 30, end="\n")  # Header of the program
+        # While loop to check if the value is between 1 and 5
+        while functionToUse != 5:
+            if functionToUse == 1:  # The first function of the program
+                equation = input("Please enter the equation: ")
+                firstFunction(equation)
+            elif functionToUse == 2:
+                secondFunction()
+            elif functionToUse == 3:
+                thirdFunction()
+            elif functionToUse == 4:
+                settingsFunction()
 
-    # Print the lists of function that the user want to use
-    print("Lists of functions:")
-    print("1. Basic Equations\n2. Combinatorics\n3. Calculus\n4. Settings\n5. Exit the calculator\n")
+            listOfFunc() # Loop lists until exit
 
-    functionToUse = 0 # Set value to 0 to make the while loop work
-    while functionToUse != 1 or functionToUse != 2 or functionToUse != 3 or functionToUse != 4 or functionToUse != 5: # While loop to check if the value is between 1 and 5
-        functionToUse = int(input("Please enter the number that corresponds to the function that you want to use: "))
-        if 1 <= functionToUse <= 3:
-            print()
-            break # If the value is between 1 and 3
-        elif functionToUse == 4:
-            settingsFunction()
-        elif functionToUse == 5: # Always need the user's consent to exit the program
-            exitFunction()
-            break
-        else:
-            print("Wrong input! Try again!\n") # If the value is not between 1 and 5. Repeat.
+            # User input whether to choose which function to use
+            functionToUse = int(input("Enter your choice: "))
 
-    if functionToUse == 1: # The first function of the program
-        equation = input("Please enter the equation: ")
-        firstFunction(equation)
-    elif functionToUse == 2:
-        secondFunction()
-    elif functionToUse == 3:
-        thirdFunction()
+            if 1 <= functionToUse <= 3:
+                print()
+            elif functionToUse == 5: # User's consent to exit the program
+                exitFunction()
+            else:
+                # If the value is not between 1 and 5. Repeat.
+                print("Wrong input! Try again!\n")
 
-    print("=" * 30 + " CALCULATOR " + "=" * 30, end="\n")  # Footer of the program
-else: # If the user's input is not 1 then execute a goodbye statement
-    exitFunction()
+        # Footer of the program
+        print("=" * 30 + " CALCULATOR " + "=" * 30, end="\n")
+    else:
+        # If the user's input is not 1 then execute a goodbye statement
+        exitFunction()
