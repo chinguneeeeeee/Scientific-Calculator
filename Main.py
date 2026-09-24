346bd498656ce0daef328b0684e76aa5e49b6256
chingunee Calculator <ctuguldur3036@eagle.fgcu.edu>
2026-09-23 20:25:12 -0400

    polishing stuff

--- Main.py
+++ Main.py
@@ -9,37 +9,36 @@ __author__ = "Chinguun"
 # Default the decimal precision to 2 and subject to change from user
 decimal_precision = 2
 
+
 def listOfFunc(index):
     # Print the lists of function that the user want to use
-    if index == 1:
+    if index == 1:  # Main menu
         print("\nLists of functions:")
         print("1. Basic Equations\n"
               "2. Combinatorics\n"
               # "3. Calculus\n"
               "4. Settings\n"
               "5. Exit the calculator\n")
-    elif index == 2:
+    elif index == 2:  # Combination menu
         print("\nLists of combinatorics functions:")
-        print("1. n!\n"
-              "2. C (n, r)\n"
-              "3. P (n, r)\n"
-              "4. HCF (Highest Common Factor)\n"
-              "5. LCD (Lowest Common Denominator)\n"
-              "6. Help\n"
-              "7. Go back to the main menu\n")
-    elif index == 4:
+        print("1. Factorial (n!)\n"
+              "2. Combination (nCr)\n"
+              "3. Permutation (nPr)\n"
+              "4. Help\n"
+              "5. Go back to the main menu\n")
+    elif index == 4:  # Settings menu
         print("Settings")
         print()
 
 
 def firstFunction(equations):
     # Return the value of the user's equation
-    answer = eval(equations)
     print()
     try:
         # eval method is considered dangerous code
         # Because of user's input does not close the brackets
         # Or misinput could cause the program to fully stop
+        answer = eval(equations)
         print(f"{equations} = {answer:.{decimal_precision}f}")
     except ValueError:
         print("Invalid Input. Value Error.")
@@ -48,7 +47,7 @@ def firstFunction(equations):
     # This function is done
 
 
-def factorial(n):
+def factorial(n):  # A simple recursive factorial function
     if n <= 1:
         return 1
     else:
@@ -56,54 +55,90 @@ def factorial(n):
 
 
 def nCr(n, r):
-    down = n - r
+    # Initializing the variables
     high = 1
     low = 1
+    down = n - r
 
+    # Check which one is the highest
+    # And change its positions
     if down > r:
         temp = r
         r = down
         down = temp
-
+    # Calculations using for loop
     for i in range(n, r, -1):
         high = high * i
     for i in range(1, down + 1):
         low = low * i
-
+    # Use floor division for integer data type
     answer = high // low
-
+    # print with formatting
     print(f"{n}C{r} is equal to {answer}")
+
+
+def nPr(n, r):
+    if n == r:  # n - r = 0, 0! = 1
+        answer = factorial(n)
+        print(f"{n}P{r} is equal to {answer}")
+    else:
+        down = n - r
+        answer = factorial(n) // factorial(down)
+        print(f"{n}P{r} is equal to {answer}")
 
 
 def secondFunction():
     to_choose = 0
 
-    while to_choose != 7:
+    while to_choose != 5:
         listOfFunc(2)
+        # User input to choose the method
         to_choose = int(input("Select the method to use: "))
 
-        if to_choose == 7:
-            print("Going back.")
-
-        if to_choose == 1:
+        if to_choose == 1:  # n! - Factorial
             n = int(input("Enter the number (n): "))
             print(f"{n}! is equal to {factorial(n)}")
-        elif to_choose == 2:
+        elif to_choose == 2:  # nCr - Combination
             n = int(input("Please enter the number (n): "))
             r = int(input("Please enter the number (r): "))
-            if n < r:
+            print()
+
+            if n > r:  # n should be higher than r
                 nCr(n, r)
-            elif n == r:
+            elif n == r:  # theoretically if n is equal to r it will always be 1
                 print(f"{n}C{r} is equal to 1")
             else:
-                print("n is higher than r. Invalid.")
+                print("r is higher than n. Invalid input.")
+        elif to_choose == 3:  # nPr - Permutation
+            n = int(input("Please enter the number (n): "))
+            r = int(input("Please enter the number (r): "))
+            print()
+
+            if n > r:  # n should be higher than r
+                nCr(n, r)
+            else:
+                print("r is higher than n. Invalid input.")
+        elif to_choose == 4:  # Instructions for the user
+            print("\n1. The factorial (n! = n! * (n-1))\n"
+                  "Example: 5! = 5*4*3*2*1 = 120\n"
+                  "2. Combination of 2 natural numbers (nCr = n!/r!*(n-r)!)\n"
+                  "Example: 3C2 = 3!/2!*1! = 3\n"
+                  "3. Permutation of 2 natural numbers (nPr = n!/(n-r)!)\n"
+                  "Example: 7P7 = 7!/(7-7)! = 5040.\n"
+                  "Keep in note that, 0! = 1, n should always be higher than r")
+        if to_choose == 5:  # Going back to the main menu
+            print("\nGoing back.")
+
 
 def settingsFunction():
-    print("Work In Progress\n")
+    # TODO add adjustable decimal pointer
+    to_choose = 0
 
 
 def exitFunction(name):
-    print("Goodbye, " + name + "! Have a nice day!")
+    # Function that prints goodbye statement
+    print("\nGoodbye, " + name + "! Have a nice day!")
+
 
 if __name__ == "__main__":
     # Asking the user's name for the introductory part
@@ -130,14 +165,14 @@ if __name__ == "__main__":
             # User input whether to choose which function to use
             functionToUse = int(input("Enter your choice: "))
 
-            if functionToUse == 1:  # The first function of the program
+            if functionToUse == 1:  # Equation function
                 equation = input("Please enter the equation: ")
                 firstFunction(equation)
-            elif functionToUse == 2:
+            elif functionToUse == 2:  # Combination function
                 secondFunction()
-            # elif functionToUse == 3:
+            # elif functionToUse == 3: # Calculus function
             #    thirdFunction()
-            elif functionToUse == 4:
+            elif functionToUse == 4:  # Settings function
                 settingsFunction()
 
             if functionToUse == 5:  # User's consent to exit the program
